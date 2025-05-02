9d4e186b126fa68a9527f080ac4766f77764e307
Denes <ldenes096@gmail.com>
2025-05-02 22:39:09 +0200

    Remove code from console

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\PropertyType;
 use App\Models\Settlement;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
