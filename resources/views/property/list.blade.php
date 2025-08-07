7432b73a226ae5e647c52d4693bb5233ca88d390
Kanalas Richard <webdeveloper3@elitecosmetix.com>
2025-08-07 11:27:16 +0200

    lista oldal design

--- resources/views/property/list.blade.php
+++ resources/views/property/list.blade.php
@@ -7,7 +7,7 @@
 			Property Listing Six
 		=====================================================
 		-->
-    <div class="property-listing-six bg-pink-two pt-110 md-pt-80 pb-150 xl-pb-120 mt-150 xl-mt-120">
+    <div class="property-listing-six bg-white py-3">
         <div class="container container-large">
             <div class="row">
                 <!--
@@ -20,13 +20,13 @@
                     <div class="container container-large">
                         <div class="position-relative z-1">
                             <div class="title-one mb-60 lg-mb-40 wow fadeInUp d-flex justify-content-between">
-                                <h2 class="font-garamond col-6">Ingatlan ajánlataink</h2>
+                                <h2 class="fs-4 ms-md-5  mt-0">Ingatlan ajánlataink</h2>
                             </div>
 
                             <div class="row gx-xxl-5">
                                     <!-- /.title-one -->
                                     @foreach($properties as $property)
-                                    <div class="col-lg-4 col-md-6">
+                                    <div class="col-lg-3 col-md-6">
                                         @include('includes/property-list-item')
                                     </div>
                                 @endforeach
