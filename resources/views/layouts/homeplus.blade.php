f990810aa9993ca48b004c49a16162674b7feab2
Kanalas Richard <webdeveloper3@elitecosmetix.com>
2025-08-15 12:42:34 +0200

    design

--- resources/views/layouts/homeplus.blade.php
+++ resources/views/layouts/homeplus.blade.php
@@ -67,6 +67,27 @@
         .listing-card-one{
             background-color: #f4f4f4 !important;
         }
+        #dont-hide{
+            color: #000;
+        }
+        .select2-container--default .select2-selection--multiple{
+            border: 1px solid #000 !important;
+            border-radius: 0.5rem !important;
+        }
+        .select2-container--default .select2-selection--multiple .select2-selection__choice{
+            background-color: #96006B !important;
+            color: #fff !important;
+        }
+        .select2-container--default .select2-selection--multiple .select2-selection__choice__remove{
+            color: #fff !important;
+        }
+        #dont-hide input {
+            border: 1px solid #000 !important;
+            border-radius: 0.5rem !important;
+        }
+        .select2-results__option{
+            color: #000 !important;
+        }
     </style>
 
     <!-- Fix Internet Explorer ______________________________________-->
